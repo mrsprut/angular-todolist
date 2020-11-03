237624b0084daa2f41c6059d060107e7df1a6058
YuriiTrofimenko <tyaa@ukr.net>
2020-11-03 20:41:32 +0200

    routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,31 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { ToDoItemsComponent } from './to-do-items/to-do-items.component';
+import { ToDoItemsComponent } from './pages/to-do-items/to-do-items.component';
 import {FormsModule} from '@angular/forms';
 import {HttpClientModule} from '@angular/common/http';
+import { AboutComponent } from './pages/about/about.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
+import {RouterModule, Routes} from '@angular/router';
+
+const routes: Routes = [
+  { path: '', component: ToDoItemsComponent, },
+  { path: 'about', component: AboutComponent },
+  { path: '**', component: NotFoundComponent }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    ToDoItemsComponent
+    ToDoItemsComponent,
+    AboutComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/pages/to-do-items/to-do-items.component.ts
+++ src/app/pages/to-do-items/to-do-items.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { ToDoItem } from '../to-do-item';
-import { TodoItemsService } from '../todo-items.service';
+import { ToDoItem } from '../../to-do-item';
+import { TodoItemsService } from '../../todo-items.service';
 import {delay} from 'rxjs/operators';
 
 @Component({
